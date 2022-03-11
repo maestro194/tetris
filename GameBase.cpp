72965f2653fd99a35148a303a706a74f0ef5ee35
maestro194 <dongboy1904@gmail.com>
2022-03-11 19:00:27 +0700

    Update GameBase.cpp

--- GameBase.cpp
+++ GameBase.cpp
@@ -1,10 +1,13 @@
 #include "GameBase.h"
 
-void ErrorLog(std::string msg, int error_code) {
-	if (error_code == SDL_ERROR) {
-		std::cout << msg << " " << SDL_GetError() << '\n';
+void ErrorLog(std::string msg, int error_code)
+{
+	if (error_code == SDL_ERROR)
+	{
+		std::cout << msg << SDL_GetError() << std::endl;
 	}
-	if (error_code == IMG_ERROR) {
-		std::cout << msg << " " << IMG_GetError() << '\n';
+	if (error_code == IMG_ERROR)
+	{
+		std::cout << msg << IMG_GetError() << std::endl;
 	}
 }
