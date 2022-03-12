32a24ba4c2e136459cc2959c6285c13adaad6ac6
maestro194 <dongboy1904@gmail.com>
2022-03-12 07:58:51 +0700

    Update button

--- Button.cpp
+++ Button.cpp
@@ -1,13 +1,53 @@
 #include "Button.h"
 
 Button::Button() {
+	buttonSprite = BUTTON_DEFAULT;
+	buttonPosition.x = 0;
+	buttonPosition.y = 0;
+}
 
+Button::Button(int x, int y, int w, int h) {
+	buttonSprite = BUTTON_DEFAULT;
+	buttonPosition.x = x;
+	buttonPosition.y = y;
+	buttonWidth = w;
+	buttonHeight = h;
 }
 
 Button::~Button() {
 
 }
 
-Button::DrawButton(int x, int y, ) {
+bool Button::CursorOnButton(int x, int y) {
+	bool OnButton = true;
+	if (x < buttonPosition.x)
+		OnButton = false;
+	if (x > buttonPosition.x + buttonWidth)
+		OnButton = false;
+	if (y < buttonPosition.y)
+		OnButton = false;
+	if (y > buttonPosition.y + buttonHeight)
+		OnButton = false;
 
+	return OnButton;
+}
+
+void Button::Render(SDL_Renderer* renderer, Texture buttonTexture, SDL_Rect* clip) {
+	buttonTexture.Render(renderer, buttonPosition.x, buttonPosition.y, clip);
+}
+
+void Button::HandleEvent(SDL_Event* e) {
+	if (e->type == SDL_MOUSEMOTION || e->type == SDL_MOUSEBUTTONDOWN || e->type == SDL_MOUSEBUTTONUP) {
+		int x, y;
+		SDL_GetMouseState(&x, &y);
+
+		if (CursorOnButton(x, y)) {
+			if (e->type == SDL_MOUSEMOTION) 
+				buttonSprite = BUTTON_HOVERED;
+			if (e->type == SDL_MOUSEBUTTONDOWN)
+				buttonSprite = BUTTON_DOWN;
+		}
+		else
+			buttonSprite = BUTTON_DEFAULT;
+	}
 }
--- Button.h
+++ Button.h
@@ -1,14 +1,24 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
+#include "GameBase.h"
+#include "Texture.h"
+
 class Button {
 public:
 	Button();
+	Button(int x, int y, int w, int h);
 	~Button();
 
-	void DrawButton(int x, int y);
+	int buttonSprite;
+	
+	bool CursorOnButton(int x, int y);
+	void Render(SDL_Renderer* renderer, Texture ButtonTexture, SDL_Rect* clip);
+	void HandleEvent(SDL_Event* e);
 private:
-
+	SDL_Point buttonPosition;
+	int buttonWidth,
+			buttonHeight;
 };
 
 #endif
